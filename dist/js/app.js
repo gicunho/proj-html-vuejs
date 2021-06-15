92b60c2305b170037c70bebf348a894a1c97c517
gicunho <gicunho@hotmail.it>
2021-06-15 23:56:46 +0200

    Completato Sezione Career

--- dist/js/app.js
+++ dist/js/app.js
@@ -23,7 +23,20 @@ var myapp = new Vue({
   el: '#root',
   data: {
     navbarLinks: ['Home', 'Pages', 'Courses', 'Features', 'Blog', 'Shop'],
-    introInfos: [new introInfo('/dist/img/home-5-image-box-01.png', 'Idea Discussion', 'Get teamed up with the specialists who work and teach coding for years at famous universities.'), new introInfo('/dist/img/home-5-image-box-02.png', 'Web Development', 'Learn to start building a webpage from scratch. You decide your own pace, course and speed.'), new introInfo('/dist/img/home-5-image-box-03.png', 'System Administration', 'Learners are encouraged to study the mechanism and structure of system administration.'), new introInfo('/dist/img/home-5-image-box-04.png', 'Graphic Design', 'Have a passion for graphics and arts? Show your talents with confidence and self-assertiveness.')]
+    introInfos: [new introInfo('/dist/img/home-5-image-box-01.png', 'Idea Discussion', 'Get teamed up with the specialists who work and teach coding for years at famous universities.'), new introInfo('/dist/img/home-5-image-box-02.png', 'Web Development', 'Learn to start building a webpage from scratch. You decide your own pace, course and speed.'), new introInfo('/dist/img/home-5-image-box-03.png', 'System Administration', 'Learners are encouraged to study the mechanism and structure of system administration.'), new introInfo('/dist/img/home-5-image-box-04.png', 'Graphic Design', 'Have a passion for graphics and arts? Show your talents with confidence and self-assertiveness.')],
+    companyNumbers: [{
+      desc: 'finished sessions',
+      numb: '1.926'
+    }, {
+      desc: 'enrolled learners',
+      numb: '3.092+'
+    }, {
+      desc: 'online instructors',
+      numb: '200'
+    }, {
+      desc: 'satisfaction rate',
+      numb: '100%'
+    }]
   },
   methods: {}
 });
