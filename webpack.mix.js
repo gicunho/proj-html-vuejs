48d2d18b72ea8d60ae31d24750db2bf202330449
gicunho <gicunho@hotmail.it>
2021-06-15 20:41:29 +0200

    Completato Header

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,9 @@
 let mix = require('laravel-mix');
-
+processCssUrls: false;
 mix
 .js('src/app.js', 'js')
 .sass('src/app.scss', 'css')
+.options({
+    processCssUrls: false
+ })
 .setPublicPath('dist');
