1028d451142bddc047ad120f48995a452cd77264
gicunho <gicunho@hotmail.it>
2021-06-16 10:33:45 +0200

    Completato Sezione Courses

--- dist/js/app.js
+++ dist/js/app.js
@@ -16,8 +16,31 @@ var introInfo = function introInfo(img, title, text) {
   this.title = title;
   this.text = text;
 };
-/* Vue */
 
+;
+
+var course = function course(name, price, teacher, teacherPicture, lessons, students, img) {
+  _classCallCheck(this, course);
+
+  this.name = name;
+  this.price = price;
+  this.teacher = teacher;
+  this.teacherPicture = teacherPicture;
+  this.lessons = lessons;
+  this.students = students;
+  this.img = img;
+};
+
+;
+var bFields = {
+  name: 'Blanche Fields',
+  pic: '/dist/img/73ee246daf47502812ccefc84bf02898 (1).jpeg'
+};
+var mStrickland = {
+  name: 'Maggie Strickland',
+  pic: '/dist/img/d0d504142acfde820eef2f11feea6253 (1).jpeg'
+};
+/* Vue */
 
 var myapp = new Vue({
   el: '#root',
@@ -36,7 +59,8 @@ var myapp = new Vue({
     }, {
       desc: 'satisfaction rate',
       numb: '100%'
-    }]
+    }],
+    courses: [new course('Learning to Write as a Professional Author', '$40.00', bFields.name, bFields.pic, 20, 50, '/dist/img/course-02-480x298.jpg'), new course('Customer-centric Info-Tech Strategies', 'Free', mStrickland.name, mStrickland.pic, 24, 769, '/dist/img/stock-full-hd-03-480x298.jpg'), new course('Open Programming Courses for Everyone: Python', '$19.00', mStrickland.name, mStrickland.pic, 17, 62, '/dist/img/stock-full-hd-04-480x298.jpg'), new course('Academic Listening and Note-taking', '$26.00', bFields.name, bFields.pic, 14, 67, '/dist/img/stock-full-hd-06-480x298.jpg'), new course('Master jQuery in a Short Period of Time', '$39.00', bFields.name, bFields.pic, 6, 51, '/dist/img/course-featured-image-01-480x298.jpg'), new course('Introduction to Javascript for Beginners', '$59.00', bFields.name, bFields.pic, 14, 76, '/dist/img/stock-full-hd-05-480x298.jpg')]
   },
   methods: {}
 });
--- src/app.js
+++ src/app.js
@@ -4,7 +4,26 @@ class introInfo {
         this.title = title;
         this.text = text;
     }
-}
+};
+class course {
+    constructor(name, price, teacher, teacherPicture, lessons, students, img) {
+        this.name = name;
+        this.price = price;
+        this.teacher = teacher;
+        this.teacherPicture = teacherPicture;
+        this.lessons = lessons;
+        this.students = students;
+        this.img = img
+    }
+};
+const bFields = {
+    name: 'Blanche Fields',
+    pic: '/dist/img/73ee246daf47502812ccefc84bf02898 (1).jpeg'
+};
+const mStrickland = {
+    name: 'Maggie Strickland',
+    pic: '/dist/img/d0d504142acfde820eef2f11feea6253 (1).jpeg'
+} 
 
 /* Vue */
 const myapp = new Vue({
@@ -34,6 +53,14 @@ const myapp = new Vue({
                 desc: 'satisfaction rate',
                 numb: '100%'
             }
+        ],
+        courses: [
+            new course ('Learning to Write as a Professional Author', '$40.00', bFields.name, bFields.pic, 20, 50, '/dist/img/course-02-480x298.jpg'),
+            new course ('Customer-centric Info-Tech Strategies', 'Free', mStrickland.name, mStrickland.pic, 24, 769, '/dist/img/stock-full-hd-03-480x298.jpg'),
+            new course ('Open Programming Courses for Everyone: Python', '$19.00', mStrickland.name, mStrickland.pic, 17, 62, '/dist/img/stock-full-hd-04-480x298.jpg'),
+            new course ('Academic Listening and Note-taking', '$26.00', bFields.name, bFields.pic, 14, 67, '/dist/img/stock-full-hd-06-480x298.jpg'),
+            new course ('Master jQuery in a Short Period of Time', '$39.00', bFields.name, bFields.pic, 6, 51, '/dist/img/course-featured-image-01-480x298.jpg'),
+            new course ('Introduction to Javascript for Beginners', '$59.00', bFields.name, bFields.pic, 14, 76, '/dist/img/stock-full-hd-05-480x298.jpg'),
         ]
     },
     methods: {
